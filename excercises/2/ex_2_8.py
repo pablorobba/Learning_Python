9584179054aaad68c175db648684d0f4b22edc4e
Pablo Robba <pablorobba1998@outlook.com>
2023-01-28 14:11:33 -0300

    finalicé los ejercicios del capitulo 2

---
+++ excercises/2/ex_2_8.py
@@ -0,0 +1,5 @@
+print (5+3)
+print (10-2)
+print (2 * 2 * 2)
+print (16 / 2)
+#tienen que dar los 4 el numero 8
---
+++ excercises/2/ex_2_9.py
@@ -0,0 +1,5 @@
+favorito = 7
+mi_favorito= "mi numero favorito es " + str(favorito)
+print(mi_favorito)
+#se plantea una variable con un numero, luego otra 
+# con una frase y el numero hecho string
