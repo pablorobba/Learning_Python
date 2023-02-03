5a9007953c2157919cc8d89f15dcbfeee130148a
Pablo Robba <pcrcovres@gmail.com>
2023-02-02 23:38:54 -0300

    empecé el cap 6

---
+++ excercises/6/6_3.py
@@ -0,0 +1,13 @@
+glossary = {
+    "variable": "holds a value",
+    "method": "action can perform on a piece of data",
+    "list": "collection of items in a particular order",
+    "tuple": "list that are inmutable",
+    "dictionaries": "coleccion of key values",
+}
+
+print("\nthe variable is " + glossary ["variable"] +
+      "\nthe method is " + glossary["method"] +
+      "\nthe list is " + glossary["list"] +
+      "\nthe tuple is " + glossary["tuple"] + 
+      "\nthe dictionaries are a " + glossary["dictionaries"])
